12afd3df1a6eecb2b43b7bd150d866c61f2f5e5f
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2022-12-19 12:25:32 -0300

    Add files via upload

---
+++ nascer.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+
+#atividade   
