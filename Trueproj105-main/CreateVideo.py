20deff63558a10dbe0493ed536aaeca75be6255a
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2024-09-14 13:55:43 -0300

    CreateVideo.py

--- Trueproj105-main/CreateVideo.py
+++ Trueproj105-main/CreateVideo.py
@@ -5,26 +5,3 @@ path = "Images"
 
 images=[]
 
-for file in os.listdir(path):
-    name, ext = os.path.splitext(file)
-
-    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
-        file_name = path+"/"+file
-
-        print(file_name)
-               
-        images.append(file_name)
-        
-count = len(images)
-
-frame = cv2.imread(images[0])
-height, width, channels = frame.shape
-size = (width,height ) 
-print(size)
-
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
-
-#crie o for para executar a leitura de video
-
-out.release()
-print("Concluído")
