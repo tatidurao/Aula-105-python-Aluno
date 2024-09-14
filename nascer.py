07f3d46a72289fed3f02c4ba0849d84a628f9815
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2024-09-14 13:56:04 -0300

    nascer.py

--- nascer.py
+++ nascer.py
@@ -7,17 +7,4 @@ path = "Images"
 images = []
 
 
-for file in os.listdir(path):
-    name, ext = os.path.splitext(file)
-
-    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
-        file_name = path+"/"+file
-
-        print(file_name)
-               
-        images.append(file_name)
-        
-print(len(images))
-count = len(images)
-
 #atividade   
